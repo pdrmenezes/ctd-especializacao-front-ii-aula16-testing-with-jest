253df6d580f1bbd02806e0b1dd1377a34128ed62
pdrmenezes <pdrmenezes1@gmail.com>
2022-12-06 21:23:09 -0300

    creating tests

--- src/files/dummy.spec.ts
+++ src/files/dummy.spec.ts
@@ -2,9 +2,17 @@ import dummy from "../files/dummy";
 
 describe("dummy", () => {
   describe("when shouldTrigger is false", () => {
-    it("should not call callback", () => {});
+    it("should not call callback", () => {
+      const mockCallback = jest.fn();
+      dummy(false, mockCallback);
+      expect(mockCallback).not.toBeCalled();
+    });
   });
   describe("when shouldTrigger is true", () => {
-    it("should call callback", () => {});
+    it("should call callback", () => {
+      const mockCallback = jest.fn();
+      dummy(true, mockCallback);
+      expect(mockCallback).toBeCalled();
+    });
   });
 });
--- src/files/game.spec.ts
+++ src/files/game.spec.ts
@@ -1,5 +1,8 @@
 import game from "../files/game";
 
 describe("game", () => {
-  it("game should return the right object", () => {});
+  it("game should return the right object", () => {
+    const result = game("Pedro");
+    expect(result).toStrictEqual({ id: 1, name: "Pedro", description: "First game" });
+  });
 });
